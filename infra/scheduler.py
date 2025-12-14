a23dff7b0bb4dccaf3652ae66d1fcfcf99ffa390
BikTaIl <tbikmuxametov@list.ru>
2025-12-15 01:30:20 +0300

    added logging into scheduler

--- infra/scheduler.py
+++ infra/scheduler.py
@@ -5,14 +5,23 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 from commands.sync import sync_function
 from models.gh_client import GitHubClassroomClient
+from models.db import ErrorLog
 from db import AsyncSessionLocal
 
 scheduler = AsyncIOScheduler()
 gh = GitHubClassroomClient()
 
+
 async def check_github():
-    async with AsyncSessionLocal() as session:
-        await sync_function(session)
+    try:
+        async with AsyncSessionLocal() as session:
+            await sync_function(session)
+    except Exception as e:
+        async with AsyncSessionLocal() as error:
+            async with error.begin():
+                error.add(ErrorLog(
+                    message=str(e)
+                ))
 
 
 async def check_internal_tables():
