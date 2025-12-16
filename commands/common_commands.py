08768625b0a3a6a317ba460177c40a6a30638e0e
polushari <Polushari@ya.ru>
2025-12-16 05:09:29 +0300

    commands done

---
+++ commands/common_commands.py
@@ -0,0 +1,43 @@
+from typing import Optional, Sequence, Mapping, Any
+from datetime import datetime, date
+from sqlalchemy import select, update, delete, and_, or_, func
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
+from decimal import Decimal
+
+from models.db import User, GithubAccount, Notification, Course, Assignment, Assistant, Submission, Permission, \
+    ErrorLog, AccessDenied
+
+
+async def login_link_github(telegram_id: int, session: AsyncSession) -> str:
+    """Вернуть URL для привязки GitHub-аккаунта к пользователю Telegram."""
+    pass
+
+
+async def complete_github_link(telegram_id: int, code: str, state: str, session: AsyncSession) -> None:
+    """Завершить привязку GitHub-аккаунта после редиректа от GitHub"""
+    pass
+
+
+async def logout_user(telegram_id: int, session: AsyncSession) -> None:
+    pass
+
+
+async def set_active_role(telegram_id: int, role: str, session: AsyncSession) -> None:
+    """Установить активную роль пользователя: 'student', 'teacher', 'assistant', 'admin'. С проверкой на доступность роли"""
+    pass  # жду деталей реализации
+
+
+async def toggle_global_notifications(telegram_id: int, session: AsyncSession) -> None:
+    """Глобально сменить рычажок уведомлений по дд для пользователя."""
+    user = await session.get(User, telegram_id)
+    if user:
+        user.notifications_enabled = not user.notifications_enabled
+        await session.commit()
+    else:
+        raise ValueError(f"Пользователя {telegram_id} не существует.")
+
+
+async def change_git_account(telegram_id: int, github_login: str, session: AsyncSession) -> None:
+    """Сменить гитхаб-аккаунт на другой залогиненный"""
+    pass
